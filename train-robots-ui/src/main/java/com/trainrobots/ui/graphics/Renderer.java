5cae891b414e607833865e4053d10c4269367688
Kais <kais@kaisdukes.com>
2013-08-25 12:24:19 +0100

    JOGL chessboard.

--- train-robots-ui/src/main/java/com/trainrobots/ui/graphics/Renderer.java
+++ train-robots-ui/src/main/java/com/trainrobots/ui/graphics/Renderer.java
@@ -19,34 +19,70 @@ package com.trainrobots.ui.graphics;
 
 import javax.media.opengl.GL;
 import javax.media.opengl.GL2;
-import javax.media.opengl.glu.GLU;
 
 public class Renderer {
 
 	public static void setup(GL2 gl2, int width, int height) {
 
-		gl2.glMatrixMode(GL2.GL_PROJECTION);
-		gl2.glLoadIdentity();
+		// TODO: INITIATE VIEW PORT!
 
-		GLU glu = new GLU();
-		glu.gluOrtho2D(0.0f, width, 0.0f, height);
-
-		gl2.glMatrixMode(GL2.GL_MODELVIEW);
-		gl2.glLoadIdentity();
-
-		gl2.glViewport(0, 0, width, height);
+		// gl2.glMatrixMode(GL2.GL_PROJECTION);
+		// gl2.glLoadIdentity();
+		//
+		// GLU glu = new GLU();
+		// glu.gluOrtho2D(0.0f, width, 0.0f, height);
+		//
+		// gl2.glMatrixMode(GL2.GL_MODELVIEW);
+		// gl2.glLoadIdentity();
+		//
+		// gl2.glViewport(0, 0, width, height);
 	}
 
-	public static void render(GL2 gl2, int width, int height) {
-		gl2.glClear(GL.GL_COLOR_BUFFER_BIT);
-		gl2.glLoadIdentity();
-		gl2.glBegin(GL.GL_TRIANGLES);
-		gl2.glColor3f(1, 0, 0);
-		gl2.glVertex2f(0, 0);
-		gl2.glColor3f(0, 1, 0);
-		gl2.glVertex2f(width, 0);
-		gl2.glColor3f(0, 0, 1);
-		gl2.glVertex2f(width / 2, height);
-		gl2.glEnd();
+	public static void render(GL2 gl, int width, int height) {
+		float x, y;
+		int steps = 8;
+		double size = 0.90 / steps;
+
+		gl.glColor3f(1.f, 1.f, 1.f);
+
+		for (int i = 0; i < steps; i++) {
+			for (int j = 0; j < steps; j++) {
+				gl.glBegin(GL2.GL_QUADS);
+				x = 0.03f + 0.90f * (float) j / steps;
+				y = 0.03f + 0.90f * (float) i / steps;
+				if ((i + j) % 2 == 0) {
+					gl.glColor3f(0.0f, 0f, 0f);
+					gl.glVertex2d(x, y);
+					gl.glVertex2d(x, y + size);
+					gl.glVertex2d(x + size, y + size);
+					gl.glVertex2d(x + size, y);
+
+				} else {
+					gl.glColor3f(1, 1, 1);
+					gl.glVertex2d(x, y);
+					gl.glVertex2d(x + size, y);
+					gl.glVertex2d(x + size, y + size);
+					gl.glVertex2d(x, y + size);
+				}
+				gl.glEnd();
+
+			}
+		}
+
+		gl.glLineWidth(5);
+
+		gl.glBegin(GL.GL_LINE_LOOP);
+		gl.glColor3f(1.f, 1.f, 1.f);
+		gl.glVertex2d(0.0255, 0.0255);
+
+		gl.glColor3f(0.f, 0.f, 0.f);
+		gl.glVertex2d(.93, .0255);
+
+		gl.glColor3f(1.0f, 1.0f, 1.0f);
+		gl.glVertex2d(.930, .930);
+
+		gl.glColor3f(0.f, 0.f, 0.f);
+		gl.glVertex2d(0.0255, .930);
+		gl.glEnd();
 	}
 }
