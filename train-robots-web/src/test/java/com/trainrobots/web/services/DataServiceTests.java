74b3e3912848ccaae3e1a8983995d76e3e1500f6
Kais <kais@kaisdukes.com>
2013-09-01 09:05:57 +0100

    Remove test code.

--- train-robots-web/src/test/java/com/trainrobots/web/services/DataServiceTests.java
+++ train-robots-web/src/test/java/com/trainrobots/web/services/DataServiceTests.java
@@ -21,38 +21,11 @@ import static org.junit.Assert.assertEquals;
 
 import javax.servlet.ServletContext;
 
-import org.joda.time.DateTime;
-import org.joda.time.DateTimeZone;
-import org.joda.time.Period;
-import org.joda.time.format.PeriodFormatter;
-import org.joda.time.format.PeriodFormatterBuilder;
 import org.junit.Test;
 
 import com.trainrobots.web.game.User;
 
 public class DataServiceTests {
-
-	@Test
-	public void testFoo() {
-
-		DateTime last = new DateTime(2013, 8, 31, 01, 22, 16, 323,
-				DateTimeZone.UTC);
-		System.out.println(last);
-
-		DateTime now = new DateTime(DateTimeZone.UTC);
-		System.out.println(now);
-
-		PeriodFormatter daysHoursMinutes = new PeriodFormatterBuilder()
-				.appendDays().appendSuffix(" day", " days")
-				.appendSeparator(", ").appendHours().appendSuffix(" hour", " hours")
-				.appendSeparator(", ").appendMinutes().appendSuffix(" minute", " minutes")
-				.appendSeparator(", ").appendSeconds().appendSuffix(" second", " seconds")
-				.toFormatter();
-
-		Period p = new Period(last, now);
-		System.out.println(p);
-		System.out.println(daysHoursMinutes.print(p));
-	}
 
 	@Test
 	public void shouldGetUser() {
