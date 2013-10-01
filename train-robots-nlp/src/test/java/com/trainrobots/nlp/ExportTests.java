9df606e5d2ba5814a96375d3de7b29bb7af46f73
Kais <kais@kaisdukes.com>
2013-10-01 12:01:15 +0100

    Export treematica file.

---
+++ train-robots-nlp/src/test/java/com/trainrobots/nlp/ExportTests.java
@@ -0,0 +1,61 @@
+/* Copyright (C) Kais Dukes.
+ * Email: kais@kaisdukes.com
+ *
+ * This file is part of Train Robots.
+ *
+ * This is free software: you can redistribute it and/or modify it under the terms
+ * of the GNU General Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Train Robots. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.trainrobots.nlp;
+
+import java.util.List;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+import com.trainrobots.core.corpus.Command;
+import com.trainrobots.core.corpus.Corpus;
+import com.trainrobots.core.io.FileWriter;
+import com.trainrobots.core.nodes.Node;
+import com.trainrobots.nlp.tokenizer.Tokenizer;
+
+public class ExportTests {
+
+	@Test
+	@Ignore
+	public void shouldExportTreematicaFile() {
+
+		FileWriter writer = new FileWriter(
+				"c:/development/treematica/data/train-robots.tm");
+		writer.writeLine("## treematica");
+		writer.writeLine("## format\tRcl");
+		int count = 0;
+		for (Command command : Corpus.getCommands()) {
+			if (command.rcl != null) {
+				writer.writeLine();
+				writer.writeLine("## id\t" + command.id);
+				List<Node> tokens = Tokenizer.getTokens(command.text).children;
+				for (int i = 0; i < tokens.size(); i++) {
+					if (i > 0) {
+						writer.write('\t');
+					}
+					writer.write(tokens.get(i).getValue());
+				}
+				writer.writeLine();
+				writer.writeLine(command.rcl.toString());
+				count++;
+			}
+		}
+		writer.close();
+		System.out.println("Wrote: " + count + " commands");
+	}
+}
