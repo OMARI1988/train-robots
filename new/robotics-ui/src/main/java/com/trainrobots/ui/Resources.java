67d288892967fcf23383b6b585d76a1ca118d244
Kais Dukes <kais@xsemantics.com>
2014-05-26 19:31:39 +0100

    Fix copyright.

--- new/robotics-ui/src/main/java/com/trainrobots/ui/Resources.java
+++ new/robotics-ui/src/main/java/com/trainrobots/ui/Resources.java
@@ -1,18 +1,9 @@
-/* Copyright (C) Kais Dukes.
+/* This file is part of Train Robots (www.TrainRobots.com).
+ * 
+ * Copyright (C) Kais Dukes, 2014.
  * Email: kais@kaisdukes.com
  *
- * This file is part of Train Robots.
- *
- * This is free software: you can redistribute it and/or modify it under the terms
- * of the GNU General Public License as published by the Free Software Foundation,
- * either version 3 of the License, or (at your option) any later version.
- *
- * This software is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
- * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along with
- * Train Robots. If not, see <http://www.gnu.org/licenses/>.
+ * Released under Version 3 of the GNU General Public License (GPL).
  */
 
 package com.trainrobots.ui;
