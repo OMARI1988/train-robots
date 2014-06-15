2506eb5ad9b92aadf631cfc10298ae672a8adc35
Kais Dukes <kais@xsemantics.com>
2014-06-15 06:51:33 +0100

    Removed debugging statement.

--- robotics-ui/src/main/java/com/trainrobots/ui/services/validation/ValidationService.java
+++ robotics-ui/src/main/java/com/trainrobots/ui/services/validation/ValidationService.java
@@ -113,7 +113,6 @@ public class ValidationService {
 			text.append(size == 1 ? "error" : "errors");
 			text.append(").");
 		}
-		System.out.println("*********** [" + text + "] ***********");
 		windowService.error(text.toString());
 	}
 }
